a63fae47007df73b8a3ca55083cc5f44fa1bf496
Stanislas berlaud <157527948+StanislasBerlaud@users.noreply.github.com>
2026-02-02 22:16:33 +0100

    Add installation script for Zabbix Docker setup
    This script installs Docker, checks for an existing installation directory, and either updates it or clones a repository. It then starts the Docker containers using Docker Compose.

---
+++ install.sh
@@ -0,0 +1,22 @@
+INSTALL_DIR="zabbix-docker"
+
+echo "Préparation de l'installation automatique..."
+
+sudo apt update && sudo apt install -y git curl
+if ! command -v docker &> /dev/null; then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+fi
+
+if [ -d "$INSTALL_DIR" ]; then
+    echo "📁 Le dossier $INSTALL_DIR existe déjà. Mise à jour..."
+    cd $INSTALL_DIR && git pull
+else
+    git clone https://github.com/StanislasBerlaud/Installation-Zabbix-Serv-.git $INSTALL_DIR
+    cd $INSTALL_DIR
+fi
+
+sudo docker compose up -d
+
+echo "Accès : http://$(hostname -I | awk '{print $1}')"
